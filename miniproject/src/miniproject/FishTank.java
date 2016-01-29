4b33a8a76cc9a1c9ecc2d2b7963b27664bebc08e
KMC961 <chaitanya.cse.astra@gmail.com>
2016-01-29 22:11:15 +0530

    Update FishTank.java

--- miniproject/src/miniproject/FishTank.java
+++ miniproject/src/miniproject/FishTank.java
@@ -4,18 +4,38 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class FishTank extends JFrame{  
+public class FishTank{  
 	FishTank(){  
-		Button b1=new Button("display"); 
-		Button b2=new Button("clear");
-		b1.setBounds(10,210,70,30);
-		b2.setBounds(100,210,70,30);
-		add(b2);
-		add(b1);
-		setSize(300,300);
-		setLayout(null); 
-		setVisible(true); 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		   
+		JFrame frame = new JFrame("Frame of Tank");
+	    frame.setSize(300, 200);
+	    frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		   
+		    
+		    
+		    Button b1=new Button("display"); 
+			Button b2=new Button("clear");
+			b1.setBounds(10,210,70,30);
+			b2.setBounds(100,210,70,30);
+			
+			Panel pane1=new Panel();
+			
+			
+			
+			Panel pane2=new Panel();
+			pane2.add(b1);
+			pane2.add(b2);
+			
+			
+			
+			JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,pane1,pane2);
+		   // splitPane.setOrientation();
+		
+		    frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+		
+		
 		}  
 		public static void main(String args[]){  
 		FishTank f=new FishTank();  
